6e26e4cd4cfa58397b02c685c35d9273bc498fd1
Vera <ipeachyou@yandex.ru>
2021-08-01 17:41:05 +0500

    random в MainViewModel

--- app/src/main/java/com/example/weatherapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/MainViewModel.kt
@@ -6,18 +6,31 @@ import androidx.lifecycle.ViewModel
 import com.example.weatherapp.model.Repository
 import com.example.weatherapp.model.RepositoryImpl
 import java.lang.Thread.sleep
+import kotlin.random.Random
 
-class MainViewModel(private val liveDataObserver : MutableLiveData<AppState> = MutableLiveData(),
-                    val repository: Repository=RepositoryImpl()) : ViewModel() {
+class MainViewModel(
+    private val liveDataObserver: MutableLiveData<AppState> = MutableLiveData(),
+    val repository: Repository = RepositoryImpl()
+) : ViewModel() {
     fun getLiveData() = liveDataObserver
 
     fun getWeather() = getDataFromLocalSource()
 
     fun getDataFromLocalSource() {
+        val i: Int = 3
+        val random = Random(3).nextInt()
+        when (random) {
+            1 -> {liveDataObserver.postValue(AppState.Loading)
+            }
+            2 -> {
+                liveDataObserver.postValue(AppState.Success(repository.getWeatherFromLocal()))
+            }
+            3 -> {
+                // error
+            }
+        }
         Thread {
-            liveDataObserver.postValue(AppState.Loading)
             sleep(1000)
-            liveDataObserver.postValue(AppState.Success(repository.getWeatherFromLocal()))
         }.start()
     }
 }
